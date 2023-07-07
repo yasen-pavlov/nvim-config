a4c3b825332dcf8f4f21ecd7e8470393733978d7
Yasen Pavlov <yasen.pavlov@bitnet.me>
2023-07-07 20:18:14 +0200

    add cmp

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -4,9 +4,12 @@ if not status_ok then
   vim.notify("lualine not found!")
   return
 end
+
 lualine.setup {
   options = {
-    theme = 'material-stealth'
+    theme = 'material-stealth',
+    component_separators = { left = '|', right = '|' },
+    section_separators = { left = '', right = ''}
   }
 }
 
