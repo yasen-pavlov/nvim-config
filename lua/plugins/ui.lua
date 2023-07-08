2256e83d52adf69296ef57dbc932c6fbe0a184c8
Yasen Pavlov <yasen.pavlov@bitnet.me>
2023-07-08 19:10:48 +0200

    fix formatting

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,60 +1,62 @@
 return {
-    -- Colorscheme
-    {
-        'marko-cerovac/material.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.material_style = 'darker'
-            vim.cmd([[colorscheme material]])
-        end
-    }, 
-    
-    -- lualine
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
-        config = function()
-            require('lualine').setup {
-                options = {
-                  theme = 'material-stealth',
-                  component_separators = { left = '|', right = '|' },
-                  section_separators = { left = '', right = ''},
-                }
-            }
-        end
-    },
+  -- Colorscheme
+  {
+    'marko-cerovac/material.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.material_style = 'darker'
+      vim.cmd([[colorscheme material]])
+    end
+  },
 
-    -- barbar
-    {
-        'romgrk/barbar.nvim',
-        dependencies = {
-            'lewis6991/gitsigns.nvim',
-            'nvim-tree/nvim-web-devicons',
+  -- lualine
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
+    config = function()
+      require('lualine').setup {
+        options = {
+          theme = 'material-stealth',
+          component_separators = { left = '|', right = '|' },
+          section_separators = { left = '', right = ''},
         }
-    },
+      }
+    end
+  },
 
-    -- illuminate
-    {
-        'RRethy/vim-illuminate',
-        config = function()
-            require('illuminate').configure {
-                providers = {
-                    'lsp',
-                    'treesitter',
-                  },
-            }
-        end
-    },
-    {
-        'rcarriga/nvim-notify',
-        config = function()
-            notify = require('notify')
-            notify.setup {
-                background_colour = '#212121',
-                timeout = 3000
-            }
-            vim.notify = notify
-        end
-    },
+  -- barbar
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+      'nvim-tree/nvim-web-devicons',
+    }
+  },
+
+  -- illuminate
+  {
+    'RRethy/vim-illuminate',
+      config = function()
+        require('illuminate').configure {
+          providers = {
+            'lsp',
+            'treesitter',
+          },
+        }
+      end
+  },
+
+  -- fancy notifications
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      local notify = require('notify')
+      notify.setup {
+        background_colour = '#212121',
+        timeout = 3000
+      }
+      vim.notify = notify
+    end
+  },
 }
--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,4 +1,4 @@
 return {
-    'nvim-lua/plenary.nvim',
-    'christoomey/vim-tmux-navigator'
+  'nvim-lua/plenary.nvim', -- util lua functions
+  'christoomey/vim-tmux-navigator' -- navigation integration with tmux
 }
